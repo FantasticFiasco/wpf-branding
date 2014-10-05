afb28e17a8ae3546f265389037e307eb2a0e6cec
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-10-05 11:04:34 +0200

    Initial commit.

---
+++ WpfBranding/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+using GalaSoft.MvvmLight;
+
+namespace WpfBranding.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
