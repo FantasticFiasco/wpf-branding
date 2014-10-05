f8836b34339eb5c035ea7ea5f8fb558a49996297
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-10-05 20:43:58 +0200

    Started implementing MainWindowViewModel.

--- WpfBranding/MainViewModel.cs
+++ WpfBranding/MainViewModel.cs
@@ -1,11 +1,64 @@
+using System.Globalization;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace WpfBranding
 {
     public class MainViewModel : ViewModelBase
     {
+        private readonly ICommand numericInputCommand;
+        private readonly ICommand numericSeparatorInputCommand;
+
+        private string input;
+
         public MainViewModel()
         {
+            numericInputCommand = new RelayCommand<string>(ExecuteNumericInput);
+            numericSeparatorInputCommand = new RelayCommand(ExecuteNumericSeparatorInput);
+            input = string.Empty;
+        }
+
+        public string Input
+        {
+            get { return input; }
+            set
+            {
+                if (input == value)
+                    return;
+
+                RaisePropertyChanging();
+                input = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string NumberDecimalSeparator
+        {
+            get { return CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator; }
+        }
+
+        public ICommand NumericInputCommand
+        {
+            get { return numericInputCommand; }
+        }
+
+        public ICommand NumericSeparatorInputCommand
+        {
+            get { return numericSeparatorInputCommand; }
+        }
+
+        private void ExecuteNumericInput(string number)
+        {
+            Input += number;
+        }
+
+        private void ExecuteNumericSeparatorInput()
+        {
+            if (!Input.Contains(NumberDecimalSeparator))
+            {
+                Input += NumberDecimalSeparator;
+            }
         }
     }
 }
