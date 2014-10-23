cd06eac45363ab771d736e1095c0ebcec4fe4971
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-10-23 07:59:52 +0200

    Added documentation in the PowerShell script.

--- Source/Build.ps1
+++ Source/Build.ps1
@@ -8,24 +8,33 @@ RunBrand None
 RunBrand CompanyA
 RunBrand CompanyB
 
-# Build the solution
 Function Build
 {
+<#
+    .SYNOPSIS
+    Build the solution.
+#>
     $msbuild = "$env:windir\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe"
     $rebuild = $msbuild + " WpfBranding.sln /t:Rebuild /p:Configuration=Release /m"
     Invoke-Expression $rebuild
 }
 
-# Copy specified brand to output directory
 Function CopyBrand ($brand)
 {
+<#
+    .SYNOPSIS
+    Copy specified brand to output directory.
+#>
     New-Item -path . -Name bin\$brand -ItemType directory -Force
     Copy-Item WpfBranding\bin\Release\*.exe -Destination bin\$brand
     Copy-Item Brand.$brand\bin\Release\*.dll -Destination bin\$brand
 }
 
-# Runs the specified branded application
 Function RunBrand ($brand)
 {
+<#
+    .SYNOPSIS
+    Run the specified branded application.
+#>
     Start-Process bin\$brand\WpfBranding.exe
 }
