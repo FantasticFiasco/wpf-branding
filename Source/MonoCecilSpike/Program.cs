f8ad28f40b63a47bfdd371527f3303514cb45e31
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-10-23 21:44:21 +0200

    Wrote some spike code trying to set AssemblyCopyrightAttribute on assembly. It looks correct in ILSpy but isn't showing in File Explorer's properties.

--- Source/MonoCecilSpike/Program.cs
+++ Source/MonoCecilSpike/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace MonoCecilSpike
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Mono.Cecil;
+
+namespace MonoCecilSpike
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             // http://stackoverflow.com/questions/8388196/adding-custom-attributes-using-mono-cecil
+
+            string fileName = Path.Combine(CurrentDirectory, "WpfBranding.exe");
+            var assemblyDefinition = AssemblyDefinition.ReadAssembly(fileName);
+            ModuleDefinition module = assemblyDefinition.MainModule;
+
+            MethodReference attributeConstructor = module.Import(
+                typeof(AssemblyCopyrightAttribute).GetConstructor(new[] { typeof(string) }));
+
+            var attribute = new CustomAttribute(attributeConstructor);
+            attribute.ConstructorArguments.Add(new CustomAttributeArgument(module.TypeSystem.String, "Kalle"));
+
+            assemblyDefinition.CustomAttributes.Add(attribute);
+
+            assemblyDefinition.Write(Path.Combine(CurrentDirectory, "WpfBranding(2).exe"));
+        }
+
+        private static string CurrentDirectory
+        {
+            get { return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); }
         }
     }
 }
