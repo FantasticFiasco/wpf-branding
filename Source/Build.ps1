2090f818fe85aeb7b188e18e1885a8446ed6add7
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-10-27 07:59:56 +0100

    Marked brands in PowerShell as strings.

--- Source/Build.ps1
+++ Source/Build.ps1
@@ -3,6 +3,6 @@ Import-Module .\PowerShell\Invoke-MsBuild.psm1
 
 Invoke-MsBuild -SolutionPath "WpfBranding.sln" -MSBuildPath "$env:windir\Microsoft.NET\Framework\v4.0.30319\MsBuild.exe" -Parameters "/t:Rebuild /p:Configuration=Release /m"
 
-Copy-Brand None
-Copy-Brand CompanyA
-Copy-Brand CompanyB
+Copy-Brand "None"
+Copy-Brand "CompanyA"
+Copy-Brand "CompanyB"
