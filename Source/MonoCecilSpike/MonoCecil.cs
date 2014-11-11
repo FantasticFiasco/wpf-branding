3826b951a9e685f0d84af35bfc158c13c3880a73
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-11-11 15:42:19 +0100

    Added new class that will handle Mono.Cecil modifications.

---
+++ Source/MonoCecilSpike/MonoCecil.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Mono.Cecil;
+
+namespace MonoCecilSpike
+{
+    internal class MonoCecil
+    {
+        private readonly AssemblyDefinition assemblyDefinition;
+        private readonly ModuleDefinition module;
+        private readonly string newFileName;
+
+        internal MonoCecil(string fileName, string newFileName)
+        {
+            this.newFileName = newFileName;
+
+            assemblyDefinition = AssemblyDefinition.ReadAssembly(fileName);
+            module = assemblyDefinition.MainModule;
+        }
+
+        internal void Brand(string copyright)
+        {
+            AddAssemblyCopyright(copyright);
+
+            assemblyDefinition.Write(newFileName);
+        }
+
+        private void AddAssemblyCopyright(string copyright)
+        {
+            ConstructorInfo constructorInfo = typeof(AssemblyCopyrightAttribute)
+                .GetConstructor(new[] { typeof(string) });
+            
+            MethodReference attributeConstructor = module.Import(constructorInfo);
+
+            var attribute = new CustomAttribute(attributeConstructor);
+            attribute.ConstructorArguments.Add(new CustomAttributeArgument(module.TypeSystem.String, copyright));
+
+            assemblyDefinition.CustomAttributes.Add(attribute);
+        }
+    }
+}
