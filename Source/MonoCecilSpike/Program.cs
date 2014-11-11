8538482e12d5855429c1178093f05128e2159558
Mattias Kindborg <mattias.kindborg@gmail.com>
2014-11-11 15:42:19 +0100

    Added new class that will handle Mono.Cecil modifications.

--- Source/MonoCecilSpike/Program.cs
+++ Source/MonoCecilSpike/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
-using Mono.Cecil;
-using Vestris.ResourceLib;
 
 namespace MonoCecilSpike
 {
@@ -16,97 +13,79 @@ namespace MonoCecilSpike
             string newFileName = Path.Combine(CurrentDirectory, "WpfBranding(2).exe");
             
             string copyright = "Some copyright";
-            string company = "Some company";
+            //string company = "Some company";
 
             File.Copy(fileName, newFileName, true);
 
             //BrandResources(fileName, newFileName, copyright, company);
-            BrandAssembly(fileName, newFileName, copyright);
+            var monoCecil = new MonoCecil(fileName, newFileName);
+            monoCecil.Brand(copyright);
         }
 
-        private static void BrandResources(string fileName, string newFileName, string copyright, string company)
-        {
-            string filename = fileName;// Path.Combine(Environment.SystemDirectory, "atl.dll");
-            //Assert.IsTrue(File.Exists(filename));
-            string targetFilename = newFileName;
-            File.Copy(filename, targetFilename, true);
-            Console.WriteLine(targetFilename);
-            VersionResource existingVersionResource = new VersionResource();
-            existingVersionResource.DeleteFrom(targetFilename);
+        //private static void BrandResources(string fileName, string newFileName, string copyright, string company)
+        //{
+        //    string filename = fileName;// Path.Combine(Environment.SystemDirectory, "atl.dll");
+        //    //Assert.IsTrue(File.Exists(filename));
+        //    string targetFilename = newFileName;
+        //    File.Copy(filename, targetFilename, true);
+        //    Console.WriteLine(targetFilename);
+        //    VersionResource existingVersionResource = new VersionResource();
+        //    existingVersionResource.DeleteFrom(targetFilename);
 
-            VersionResource versionResource = new VersionResource();
-            versionResource.FileVersion = "1.2.3.4";
-            versionResource.ProductVersion = "4.5.6.7";
+        //    VersionResource versionResource = new VersionResource();
+        //    versionResource.FileVersion = "1.2.3.4";
+        //    versionResource.ProductVersion = "4.5.6.7";
 
-            StringFileInfo stringFileInfo = new StringFileInfo();
-            versionResource[stringFileInfo.Key] = stringFileInfo;
-            StringTable stringFileInfoStrings = new StringTable(); // "040904b0"
-            stringFileInfoStrings.LanguageID = 1033;
-            stringFileInfoStrings.CodePage = 1200;
-            //Assert.AreEqual(1033, stringFileInfoStrings.LanguageID);
-            //Assert.AreEqual(1200, stringFileInfoStrings.CodePage);
-            stringFileInfo.Strings.Add(stringFileInfoStrings.Key, stringFileInfoStrings);
-            stringFileInfoStrings["ProductName"] = "ResourceLib";
-            stringFileInfoStrings["FileDescription"] = "File updated by ResourceLib\0";
-            stringFileInfoStrings["CompanyName"] = "Vestris Inc.";
-            stringFileInfoStrings["LegalCopyright"] = "All Rights Reserved\0";
-            stringFileInfoStrings["EmptyValue"] = "";
-            stringFileInfoStrings["Comments"] = string.Format("{0}\0", Guid.NewGuid());
-            stringFileInfoStrings["ProductVersion"] = string.Format("{0}\0", versionResource.ProductVersion);
+        //    StringFileInfo stringFileInfo = new StringFileInfo();
+        //    versionResource[stringFileInfo.Key] = stringFileInfo;
+        //    StringTable stringFileInfoStrings = new StringTable(); // "040904b0"
+        //    stringFileInfoStrings.LanguageID = 1033;
+        //    stringFileInfoStrings.CodePage = 1200;
+        //    //Assert.AreEqual(1033, stringFileInfoStrings.LanguageID);
+        //    //Assert.AreEqual(1200, stringFileInfoStrings.CodePage);
+        //    stringFileInfo.Strings.Add(stringFileInfoStrings.Key, stringFileInfoStrings);
+        //    stringFileInfoStrings["ProductName"] = "ResourceLib";
+        //    stringFileInfoStrings["FileDescription"] = "File updated by ResourceLib\0";
+        //    stringFileInfoStrings["CompanyName"] = "Vestris Inc.";
+        //    stringFileInfoStrings["LegalCopyright"] = "All Rights Reserved\0";
+        //    stringFileInfoStrings["EmptyValue"] = "";
+        //    stringFileInfoStrings["Comments"] = string.Format("{0}\0", Guid.NewGuid());
+        //    stringFileInfoStrings["ProductVersion"] = string.Format("{0}\0", versionResource.ProductVersion);
 
-            VarFileInfo varFileInfo = new VarFileInfo();
-            versionResource[varFileInfo.Key] = varFileInfo;
-            VarTable varFileInfoTranslation = new VarTable("Translation");
-            varFileInfo.Vars.Add(varFileInfoTranslation.Key, varFileInfoTranslation);
-            varFileInfoTranslation[ResourceUtil.USENGLISHLANGID] = 1300;
+        //    VarFileInfo varFileInfo = new VarFileInfo();
+        //    versionResource[varFileInfo.Key] = varFileInfo;
+        //    VarTable varFileInfoTranslation = new VarTable("Translation");
+        //    varFileInfo.Vars.Add(varFileInfoTranslation.Key, varFileInfoTranslation);
+        //    varFileInfoTranslation[ResourceUtil.USENGLISHLANGID] = 1300;
 
-            versionResource.SaveTo(targetFilename);
-            Console.WriteLine("Reloading {0}", targetFilename);
+        //    versionResource.SaveTo(targetFilename);
+        //    Console.WriteLine("Reloading {0}", targetFilename);
 
-            VersionResource newVersionResource = new VersionResource();
-            newVersionResource.LoadFrom(targetFilename);
-            //DumpResource.Dump(newVersionResource);
+        //    VersionResource newVersionResource = new VersionResource();
+        //    newVersionResource.LoadFrom(targetFilename);
+        //    //DumpResource.Dump(newVersionResource);
 
-            //Assert.AreEqual(newVersionResource.FileVersion, versionResource.FileVersion);
-            //Assert.AreEqual(newVersionResource.ProductVersion, versionResource.ProductVersion);
+        //    //Assert.AreEqual(newVersionResource.FileVersion, versionResource.FileVersion);
+        //    //Assert.AreEqual(newVersionResource.ProductVersion, versionResource.ProductVersion);
 
-            //var versionResource = new VersionResource();
-
-
+        //    //var versionResource = new VersionResource();
 
 
-            //versionResource.LoadFrom(fileName);
-
-            //string key = new StringFileInfo().Key;
-            //var stringFileInfo  = (StringFileInfo)versionResource.Resources[key];
-            //stringFileInfo["LegalCopyright"] = copyright + "\0";
-            //stringFileInfo["CompanyName"] = company;
-
-            ////versionResource.DeleteFrom(newFileName);
-            //versionResource.SaveTo(newFileName);
 
 
-            ////versionResource.Resources
-        }
+        //    //versionResource.LoadFrom(fileName);
 
-        private static void BrandAssembly(string fileName, string newFileName, string copyright)
-        {
-            //// http://stackoverflow.com/questions/8388196/adding-custom-attributes-using-mono-cecil
+        //    //string key = new StringFileInfo().Key;
+        //    //var stringFileInfo  = (StringFileInfo)versionResource.Resources[key];
+        //    //stringFileInfo["LegalCopyright"] = copyright + "\0";
+        //    //stringFileInfo["CompanyName"] = company;
+
+        //    ////versionResource.DeleteFrom(newFileName);
+        //    //versionResource.SaveTo(newFileName);
 
 
-            var assemblyDefinition = AssemblyDefinition.ReadAssembly(fileName);
-            ModuleDefinition module = assemblyDefinition.MainModule;
-
-            MethodReference attributeConstructor = module.Import(
-                typeof(AssemblyCopyrightAttribute).GetConstructor(new[] { typeof(string) }));
-
-            var attribute = new CustomAttribute(attributeConstructor);
-            attribute.ConstructorArguments.Add(new CustomAttributeArgument(module.TypeSystem.String, copyright));
-
-            assemblyDefinition.CustomAttributes.Add(attribute);
-
-            assemblyDefinition.Write(Path.Combine(CurrentDirectory, newFileName));
-        }
+        //    ////versionResource.Resources
+        //}
 
         private static string CurrentDirectory
         {
